79c10c30fad8d7663d6a299c64bd1694d0306f08
kiruthihan nagarajan <47429159+kiruthihan10@users.noreply.github.com>
2021-12-16 15:46:04 +0530

    Core Codes with Vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <welcome v-if="page_id == 0" @move_login='page_id=2' @move_signup='page_id=1'></welcome>
+</template>
+
+<script>
+  import Welcome from './components/Welcome.vue'
+
+  // import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+  // import Vue from 'vue'
+  // Vue.use(BootstrapVue)
+  // Vue.use(IconsPlugin)
+  
+  export default {
+    name: 'App',
+    components: {
+      Welcome
+    },
+    data() {
+      return {
+        page_id : 0
+      }
+    },
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="block">
+        <section class="hero is-dark">
+            <div class="hero-body">
+                <p class="title is-size-1">
+                    e-monopoly
+                </p>
+                <p class="subtitle is-size-3">
+                    The e-Transaction for monopoly
+                </p>
+            </div>
+        </section>
+    </div>
+    <div class="container">
+        
+        <div class="block">
+            <div class="columns">
+                <div class="column">
+                    <h3 class="is-size-4">
+                        <button class="button is-success is-large" @click="$emit('move_login')">
+                            Log-IN
+                        </button>
+                    </h3>
+                </div>
+                <div class="column">
+                    <h3 class="is-size-4">
+                        <button class="button is-large is-info" @click="$emit('move_signup')">
+                            Sign-UP
+                        </button>
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    name: 'Welcome',
+    data(){
+        return {
+            login_selected : false,
+            signup_selected : false
+        }
+    },
+    emits: ['move_login', 'move_signup']
+})
+</script>
