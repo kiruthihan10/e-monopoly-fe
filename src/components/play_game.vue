38a2fd26b5827108862195bc08b96112b82c2390
kiruthihan nagarajan <47429159+kiruthihan10@users.noreply.github.com>
2021-12-24 22:07:45 +0530

    Update play_game.vue
    Make Banker a button

--- src/components/play_game.vue
+++ src/components/play_game.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="block" name="Player Info" v-if="Banker != null">
         <div class="columns">
-            <div class="column">
-                Banker of the Game {{game_id}} is {{Banker}}
+            <div class="column" @click="open_wallet(Banker)">
+                <button class="button is-link">
+                    Banker of the Game {{game_id}} is {{Banker}}
+                </button>
             </div>
             <div class="column" >
                 {{uname}}
